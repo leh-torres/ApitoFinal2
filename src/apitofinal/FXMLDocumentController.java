5916378054095df8bf844efc0a28d28e8ab8d736
Leticia Torres <leticiatp2@gmail.com>
2021-12-14 13:16:44 -0300

    Mudança de telas ao realizar o login

--- src/apitofinal/FXMLDocumentController.java
+++ src/apitofinal/FXMLDocumentController.java
@@ -10,6 +10,8 @@ import java.util.ResourceBundle;
 
 import javax.swing.JOptionPane;
 
+import classes.Usuario;
+import dao.UsuarioDao;
 import dao.VerificaDao;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -18,6 +20,9 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+import tela10_SelecionarCampeonato.Tela10SelecionarCampeonato;
+import tela4_CriarCampeonato.Tela04CriarCampeonato;
 
 /**
  *
@@ -31,45 +36,56 @@ public class FXMLDocumentController implements Initializable {
     private PasswordField senhaPass;
 
     VerificaDao verifica = new VerificaDao();
-    boolean retorno;
+    Usuario usuario = new Usuario();
+    UsuarioDao userDao = new UsuarioDao();
+    boolean retorno, retorno2;
 
     @FXML
     private void botaoEntrar(ActionEvent event) {
         retorno = verifica.verificaLogin(emailTxt.getText(), senhaPass.getText());
 
         if(retorno == true){
-            //TODO: inicializar outra tela
-            //fecha();
-            JOptionPane.showMessageDialog(null, "Login verificado.");
+            usuario.setId_usuario(userDao.getId(emailTxt.getText(), senhaPass.getText()));
+            usuario.setEmail(emailTxt.getText());
+            usuario.setSenha(senhaPass.getText());
+                
+            retorno2 = verifica.verificaCapeonato(usuario.getId_usuario());
+
+                if(retorno2 == true){
+                    Tela10SelecionarCampeonato tela10 = new Tela10SelecionarCampeonato();
+                    fecha();
+                    try {
+                        tela10.start(new Stage());
+                    } catch (Exception e) {
+                       JOptionPane.showMessageDialog(null, "Tela 10 não iniciada " + e);
+                    }
+                   
+
+                } else{
+                    Tela04CriarCampeonato tela04 = new Tela04CriarCampeonato();
+                    fecha();
+
+                    try {
+                        tela04.start(new Stage());
+                    } catch (Exception e) {
+                        JOptionPane.showMessageDialog(null, "Tela 04 não iniciada " + e);
+                    }
+
+
+                }
 
         }
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        senhaPass.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent event) ->{
-            try {
-                if(event.getCode() == KeyCode.ENTER){
-                    retorno = verifica.verificaLogin(emailTxt.getText(), senhaPass.getText());
-
-                    if(retorno == true){
-                        //TODO: inicializar outra tela
-                        //fecha();
-                        JOptionPane.showMessageDialog(null, "Login verificado.");
-
-                    }
-                }
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, e);
-            }
-        });
-        
+        //TODO fazer a interação  com o ENTER
     }  
     
     /**
      * Método para fechar a tela atual
      */
-    public void fecha(){
+    private void fecha(){
         ApitoFinal.getStage().close();
     }
     
