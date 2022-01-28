2861b4d15a7f011659b0a9716d7bab26ee32339d
Raylander524 <raylandermarques24@gmail.com>
2022-01-28 17:35:48 -0300

    Salvando as alterações do dia 28/01/2022

--- src/tela11_DefinirPartidas/DefinirPartidasFXMLController.java
+++ src/tela11_DefinirPartidas/DefinirPartidasFXMLController.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
@@ -26,6 +27,54 @@ public class DefinirPartidasFXMLController implements Initializable {
     @FXML
     private ImageView logo_usuario;
     
+    @FXML
+    private Button btn1;
+    
+    @FXML
+    private Button btn2;
+    
+    @FXML
+    private Button btn3;
+    
+    @FXML
+    private Button btn4;
+    
+    @FXML
+    private Button btn5;
+    
+    @FXML
+    private Button btn6;
+    
+    @FXML
+    private Button btn7;
+    
+    @FXML
+    private Button btn8;
+    
+    @FXML
+    private ImageView img1;
+
+    @FXML
+    private ImageView img2;
+
+    @FXML
+    private ImageView img3;
+
+    @FXML
+    private ImageView img4;
+
+    @FXML
+    private ImageView img5;
+
+    @FXML
+    private ImageView img6;
+
+    @FXML
+    private ImageView img7;
+
+    @FXML
+    private ImageView img8;
+    
     private Usuario usuario = new Usuario();
     
     /**
@@ -35,6 +84,7 @@ public class DefinirPartidasFXMLController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         nome_usuario.setText(usuario.getNome());
         logo_usuario.setImage(usuario.getImagem());
+        
         
         
     }    
