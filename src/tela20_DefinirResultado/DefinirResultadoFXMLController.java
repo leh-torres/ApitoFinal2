dcd52a2c79b8295980664a525c208cd64cba36c5
Leticia Torres <leticiatp2@gmail.com>
2022-02-15 15:18:36 -0300

    colocando pra funcionar

--- src/tela20_DefinirResultado/DefinirResultadoFXMLController.java
+++ src/tela20_DefinirResultado/DefinirResultadoFXMLController.java
@@ -99,7 +99,6 @@ public class DefinirResultadoFXMLController implements Initializable {
         int i = 0;
 
         listaJogadores = jogadoresDao.getJogadores(txtNomeJogador1.getText(), partida.getFk_time1Dinamico());
-
         if (listaJogadores.size() >= 24) {
             listaJogadores= null;
         }
